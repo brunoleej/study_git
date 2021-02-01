4454d194181371120d67e59915166934a3d39aca
leejisue <jk04059@naver.com>
2021-02-01 21:12:57 +0900

    02/01

--- ml/m22_FI_RF3_wine.py
+++ ml/m22_FI_RF3_wine.py
@@ -1,6 +1,5 @@
 # feature_importances
 # 중요도가 낮은 컬럼 제거한 후 실행 >> 제거하기 전이랑 결과 유사하다
-
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import load_wine
@@ -8,26 +7,26 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
 
-#1. DATA
-dataset = load_wine()
-x = dataset.data 
-y = dataset.target
+# Data
+wine = load_wine()
+data = wine.data 
+target = wine.target
 
-x_pd = pd.DataFrame(x, columns=dataset['feature_names']) 
-x = x_pd.drop(['ash', 'nonflavanoid_phenols', 'malic_acid'], axis=1)
-x = x.to_numpy()
+data_df = pd.DataFrame(data, columns=wine['feature_names']) 
+data = data_df.drop(['ash', 'nonflavanoid_phenols', 'malic_acid'], axis=1)
+data = data.to_numpy()
 
 x_train, x_test, y_train, y_test = \
-    train_test_split(x, y, train_size=0.8, random_state=44)
+    train_test_split(data, target, train_size=0.8, random_state=44)
 
-#2. modeling
+# Modeling
 # model = DecisionTreeClassifier(max_depth=4)
 model = RandomForestClassifier()
 
-#3. Train
+# Fitting
 model.fit(x_train, y_train)
 
-#4. Score, Predict
+# Evaluate
 acc = model.score(x_test, y_test)
 
 print("feature_importances : \n", model.feature_importances_)  
@@ -50,9 +49,9 @@ def cut_columns(feature_importances, columns, number):
 # ['ash', 'nonflavanoid_phenols', 'malic_acid']
 
 '''
-# Graph : 컬럼 중 어떤 것이 가장 중요한 것인지 보여준다.
-# 중요도가 낮은 컬럼은 제거해도 된다. >> 그만큼 자원이 절약된다.
-import matplotlib.pyplot as plt
+# Graph : 컬럼 중 어떤 것이 가장 중요한 것인지 보여줌
+# 중요도가 낮은 컬럼은 제거해도 됨 -> 자원이 절약됨
+import matp lotlib.pyplot as plt
 import numpy as np 
 
 def plot_feature_importances_dataset(model) :
@@ -68,28 +67,7 @@ plot_feature_importances_dataset(model)
 plt.show()
 '''
 
-# # DecisionTreeClassifier
 # feature_importances : 
-#  [0.         0.         0.         0.         0.01723824 0.       
-#  0.15955687 0.         0.         0.         0.05577403 0.32933594
-#  0.43809492]
-# acc :  0.8333333333333334
-
-# 중요도 0인 컬럼 제거한 후
-# feature_importances : 
-#  [0.01723824 0.15955687 0.         0.05577403 0.32933594 0.43809492]
-# acc :  0.8611111111111112
-
-# # RandomForestClassifier
-# feature_importances : 
-#  [0.12850093 0.02507761 0.01757397 0.02856365 0.02978324 0.05940561
-#  0.14125624 0.01078567 0.02434625 0.1548911  0.08333419 0.14943613
-#  0.14704543]
-# acc :  0.9722222222222222
-
-# 중요도 하위 25% 컬럼 제거
-# feature_importances : 
-#  [0.13335234 0.02338144 0.01487588 0.04290358 0.18181307 0.02349594
-#  0.11693244 0.07098034 0.15362394 0.23864105]
+#  [0.14492714 0.01782004 0.02129028 0.05216275 0.19241605 0.023224
+#  0.14684229 0.07922395 0.1296244  0.19246909]
 # acc :  0.9444444444444444
-
--- ml/m22_FI_RF4_boston.py
+++ ml/m22_FI_RF4_boston.py
@@ -1,6 +1,5 @@
 # feature_importances
 # 중요도가 낮은 컬럼 제거한 후 실행 >> 제거하기 전이랑 결과 유사하다
-
 from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.datasets import load_boston
@@ -8,26 +7,26 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
 
-#1. DATA
-dataset = load_boston()
-x = dataset.data 
-y = dataset.target
+# Data
+boston = load_boston()
+data = boston.data 
+target = boston.target
 
-x_pd = pd.DataFrame(x, columns=dataset['feature_names']) 
-x = x_pd.drop(['ZN', 'CHAS', 'RAD', 'INDUS'], axis=1)
-x = x.to_numpy()
+data_df = pd.DataFrame(data, columns=boston['feature_names']) 
+data = data_df.drop(['ZN', 'CHAS', 'RAD', 'INDUS'], axis=1)
+data = data.to_numpy()
 
 x_train, x_test, y_train, y_test = \
-    train_test_split(x, y, train_size=0.8, random_state=44)
+    train_test_split(data, target, train_size=0.8, random_state=44)
 
-#2. modeling
+# Modeling
 # model = DecisionTreeRegressor(max_depth=4)
 model = RandomForestRegressor()
 
-#3. Train
+# Fitting
 model.fit(x_train, y_train)
 
-#4. Score, Predict
+# Evaluate
 acc = model.score(x_test, y_test)
 
 print("feature_importances : \n", model.feature_importances_)  
@@ -49,8 +48,8 @@ def cut_columns(feature_importances, columns, number):
 # ['ZN', 'CHAS', 'RAD', 'INDUS']
 
 '''
-# Graph : 컬럼 중 어떤 것이 가장 중요한 것인지 보여준다.
-# 중요도가 낮은 컬럼은 제거해도 된다. >> 그만큼 자원이 절약된다.
+# Graph : 컬럼 중 어떤 것이 가장 중요한 것인지 보여줌
+# 중요도가 낮은 컬럼은 제거해도 됨 -> 자원이 절약됨
 import matplotlib.pyplot as plt
 import numpy as np 
 
@@ -67,29 +66,7 @@ plot_feature_importances_dataset(model)
 plt.show()
 '''
 
-# # DecisionTreeRegressor
 # feature_importances : 
-#  [0.04556376 0.         0.00288921 0.         0.00572861 0.61396724
-#  0.00580415 0.094345   0.         0.         0.01858958 0.00343964
-#  0.20967282]
-# acc :  0.8120351577921485
-
-# 중요도가 낮은 컬럼 제거한 후
-# feature_importances : 
-#  [0.04357642 0.61289296 0.00288416 0.09430775 0.02452501 0.00343362
-#  0.21838008]
-# acc :  0.8315993092000131
-
-# # RandomForestRegressor
-# feature_importances : 
-#  [0.0344125  0.00098024 0.00566516 0.00139049 0.02280452 0.38030136
-#  0.01436384 0.07845415 0.00409196 0.01354601 0.01855543 0.01201266
-#  0.41342167]
-# acc :  0.8940148005379513
-
-# 중요도 하위 25% 컬럼 제거
-# feature_importances : 
-#  [0.03877684 0.02540956 0.41190397 0.0159394  0.07366978 0.01695155
-#  0.02282993 0.01117619 0.38334279]
-# acc :  0.8898374136499382
-
+#  [0.03849941 0.02632476 0.40081869 0.01527202 0.07558122 0.01702458
+#  0.02138455 0.01403069 0.39106409]
+# acc :  0.8919254074404761
