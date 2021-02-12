0fe62709f403e9e42ead39d0964b09313e39889c
leejisue <jk04059@naver.com>
2021-02-12 16:30:01 +0900

    02/12

--- keras2/keras65_pipeline.py
+++ keras2/keras65_pipeline.py
@@ -1,6 +1,5 @@
 # 61 pipeline 추가
-
-# DNN과 머신러닝을 엮는다.
+# DNN & ML
 # from tensorflow.keras.wrappers.scikit_learn import KerasClassifier
 
 import numpy as np
@@ -13,17 +12,17 @@ from tensorflow.keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.model_selection import GridSearchCV, RandomizedSearchCV, KFold
 from tensorflow.keras.utils import to_categorical
 
-#1. DATA / Preprocessing
-
+# Data
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
+# Preprocessing
 y_train = to_categorical(y_train)
 y_test = to_categorical(y_test)
 
 x_train = x_train.reshape(60000, 28*28).astype('float32')
 x_test = x_test.reshape(10000, 28*28).astype('float32')
 
-#2. Modeling
+# Modelig
 def build_model(drop=0.5, optimizer='adam') :
     inputs = Input(shape=(28*28,), name='input')
     x = Dense(512, activation='relu', name='hidden1')(inputs)
@@ -37,7 +36,6 @@ def build_model(drop=0.5, optimizer='adam') :
     model.compile(optimizer=optimizer, metrics=['acc'], loss='categorical_crossentropy')
     return model
 
-
 def create_hyperparameters() :
     batches = [8, 32]
     optimizers = ['rmsprop', 'adam']
@@ -45,7 +43,6 @@ def create_hyperparameters() :
     return {"kc__batch_size" : batches, "kc__optimizer" : optimizers, "kc__drop" : dropout}
 
 hyperparameters = create_hyperparameters()
-
 model2 = KerasClassifier(build_fn=build_model, verbose=1, batch_size=32, epochs=1)   
 pipe = Pipeline([("scaler", MinMaxScaler()), ("kc", model2)])
 kf = KFold(n_splits=2, random_state=42)
