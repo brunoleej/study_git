11ecdd0b126b4f33082fa051ca6d13fd2776f96f
leejisue <jk04059@naver.com>
2021-02-12 16:31:07 +0900

    02/12

--- keras2/keras66_2_fit_generator.py
+++ keras2/keras66_2_fit_generator.py
@@ -1,6 +1,6 @@
-# ImageDataGenerator 
-# Image Preprocessing
-# fit_generator 사용 훈련
+# ImageDataGenerator 사용법 
+# 이미지 전처리
+# fit_generator Fitting
 import numpy as np
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
@@ -8,7 +8,7 @@ from tensorflow.keras.layers import Conv2D, Dense, Dropout, MaxPool2D, Flatten, 
 from keras.callbacks import EarlyStopping, ReduceLROnPlateau
 import matplotlib.pyplot as plt
 
-# ImageDataGenerator :  이미지를 증폭시켜 더 많은 데이터로 훈련시킬 수 있음
+# ImageDataGenerator : 이미지를 증폭시켜 더 많은 데이터로 훈련가능
 train_datagen = ImageDataGenerator(
     rescale=1./255,         # 전처리
     horizontal_flip=True,   # 수평
@@ -20,20 +20,19 @@ train_datagen = ImageDataGenerator(
     shear_range=0.7,        # 밀림 정도
     fill_mode='nearest'     # 빈 자리를 유사한 값으로 채움
 )
-test_datagen = ImageDataGenerator(rescale=1./255)   # 전처리만(test에서는 이미지 증폭을 할 필요 없음) 
+test_datagen = ImageDataGenerator(rescale=1./255)   # 전처리(test에서는 이미지 증폭을 할 필요가 없음)
 
-#   1) flow : 이미지가 데이터화 되어 있을 때 사용함
-#   2) flow_from_directory : 경로에 있는 파일을 데이터화
-# 폴더 자체를 라벨링할 수 있다. --> (ex) ad : 0, noraml : 1
+# flow : 이미지가 데이터화 되어 있을 때 사용
+# flow_from_directory : 경로에 있는 파일을 데이터화
 
 # train_generator
-# ad / normal (앞 0, 뒤 1로 라벨링)
-# x : (80, 150, 150, 1) -> 전부 0부터 1
-# y : (80,) -> ad : 전부 0으로 라벨링, normal : 전부 1로 라벨링
+# ad / normal (앞에 있는 걸 0, 뒤에 있는 걸 1로 라벨링됨)
+# x : (80, 150, 150, 1) -> 전부 0 ~ 1
+# y : (80,)             -> ad : 전부 0으로 라벨링 / normal : 전부 1로 라벨링
 xy_train = train_datagen.flow_from_directory(
-    '../data/image/brain/train',    # 경로에 있는 이미지들을 데이터화 (80개 이미지 전부 0으로 라벨링)
+    '../data/image/brain/train',    # 경로 설정 : 해당 폴더에 있는 이미지들을 데이터 > 80개 이미지 전부 0으로 라벨링
     target_size=(150, 150),         # 이미지 크기
-    batch_size=5,                   # batch_size 만큼의 이미지가 생성(5장씩 이미지를 자름)
+    batch_size=5,                   # batch_size 만큼의 이미지가 생성(5장씩 이미지를 자름) 
     class_mode='binary'             # 이진분류 0, 1
 )
 # Result : Found 160 images belonging to 2 classes.
@@ -41,11 +40,11 @@ xy_train = train_datagen.flow_from_directory(
 # test_generator
 xy_test = test_datagen.flow_from_directory(
     '../data/image/brain/test',    
-    target_size=(150, 150),        
-    batch_size=5,                  
+    target_size=(150, 150),         
+    batch_size=5,                   
     class_mode='binary'            
 )
-# Result : Found 120 images belonging to 2 classes.
+# 결과 : Found 120 images belonging to 2 classes.
 
 # Modeling
 model = Sequential()
@@ -78,7 +77,7 @@ model.add(Dense(64, activation='relu'))
 model.add(BatchNormalization())
 model.add(Dense(32, activation='relu'))
 model.add(BatchNormalization())
-model.add(Dense(1, activation='sigmoid'))   # 이진분류
+model.add(Dense(1, activation='sigmoid'))   
 
 es = EarlyStopping(monitor='val_loss', patience=40, mode='min')
 lr = ReduceLROnPlateau(monitor='val_loss', patience=20, factor=0.4, mode='min')
@@ -91,10 +90,10 @@ history = model.fit_generator(
     xy_train, steps_per_epoch=32, epochs=500, validation_data=xy_test, validation_steps=4, callbacks=[es, lr]
 )
 # [fit와 차이점]
-# fit_generator : x, y 뭉쳐있음
-# >> xy_train, xy_test : x, y 다 넣어줌
-# steps_per_epoch : 한epoch 당 돌아가는 횟수 -->> 전체 데이터(160) / 배치 사이즈(5) = steps_per_epoch=32
-# validation_steps : 한 epoch 종료 시 마다 검증할 때 사용되는 검증 스텝 수
+#   fit_generator : x, y 뭉쳐있음
+#   >> xy_train, xy_test : x, y 통째로 넣어줌
+#   steps_per_epoch : 한 에포 당 돌아가는 횟수 -> 전체 데이터(160) / 배치 사이즈(5) = steps_per_epoch=32
+#   validation_steps : 한 epoch 종료 시 마다 검증할 때 사용되는 검증 스텝 수
 
 acc = history.history['acc']
 val_acc = history.history['val_acc']
@@ -113,7 +112,7 @@ plt.legend(['loss','val loss','acc','val acc'])
 plt.show()
 
 print("acc : ", acc[-1])            
-print("val_acc : ", val_acc[:-1])   
+print("val_acc : ", val_acc[:-1])   # val_acc 처음부터 끝까지
 
 # acc :  0.7250000238418579
 # val_acc :  [0.6000000238418579, 0.550000011920929, 0.44999998807907104, 0.550000011920929, 0.550000011920929, 0.6499999761581421, 0.4000000059604645, 0.44999998807907104, 0.6000000238418579, 0.44999998807907104, 0.44999998807907104, 0.5, 0.949999988079071, 0.6499999761581421, 0.699999988079071, 0.8999999761581421, 1.0, 0.6499999761581421, 0.8999999761581421, 0.8999999761581421, 0.800000011920929, 0.8500000238418579, 0.8999999761581421, 0.949999988079071, 0.699999988079071, 0.8999999761581421, 0.44999998807907104, 0.800000011920929, 0.4000000059604645, 0.6499999761581421, 0.550000011920929, 0.4000000059604645, 0.5, 0.6499999761581421, 0.6000000238418579, 0.5, 0.6000000238418579, 0.6499999761581421, 0.550000011920929, 0.8999999761581421, 0.8999999761581421, 0.800000011920929, 0.8999999761581421]
