2668a152e794737714e461251992af4ad35c69da
leejisue <jk04059@naver.com>
2021-02-15 19:29:29 +0900

    02/15

---
+++ Reinforcement_learning/Project/Mario_test.py
@@ -0,0 +1,17 @@
+import gym_super_mario_bros
+import time
+import numpy as np
+from nes_py.app import JoypadSpace
+from gym_super_mario_bros.actions import RIGHT_ONLY
+from wrappers import wrapper
+
+
+env = gym_super_mario_bros.make('SuperMarioBros-1-1-v0')
+env = JoypadSpace(env.RIGHT_ONLY)
+env = wrapper(env)
+
+states = (84, 84, 4)
+actions = env.action_space.n
+
+# Agent
+agent
