f95beb210ebe4216bea536a8d0fc524c89d793c6
leejisue <jk04059@naver.com>
2021-02-15 19:28:47 +0900

    02/15

---
+++ Reinforcement_learning/Project/Mario_env.py
@@ -0,0 +1,16 @@
+import gym_super_mario_bros
+
+env = gym_super_mario_bros.make('SuperMarioBros-v0')
+
+env.reset()
+
+print(env.action_space.n)   # 256
+print(env.observation_space.shape)  # (240, 256, 3)
+
+for _ in range(10):
+    observation, reward, done, info = env.step(env.action_space.sample())
+    env.render()
+    # print(observation)
+    # print(reward)
+    # print(done)
+    print(info)
