aa6f94556b356021bca2f7002b808cf233b91139
leejisue <jk04059@naver.com>
2021-02-18 22:01:28 +0900

    02/18

---
+++ Pytorch/unsqueeze.py
@@ -0,0 +1,12 @@
+import torch
+
+x = torch.tensor([1, 2, 3, 4])
+print(torch.unsqueeze(x, 0))     # tensor([[1, 2, 3, 4]])
+
+print(torch.unsqueeze(x, 1))
+'''
+tensor([[1],
+        [2],
+        [3],
+        [4]])
+'''
