2f74f59c46b6098d069e10d8ba7a033c919cd8b0
leejisue <jk04059@naver.com>
2021-02-20 19:45:02 +0900

    02/20

---
+++ keras2/keras68_mnist_distribute.py
@@ -0,0 +1,100 @@
+# distribute
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.datasets import mnist
+
+# Data
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+# print(x_train.shape, y_train.shape) # (60000, 28, 28)--> 흑백 1 생략 가능 (60000,) 
+# print(x_test.shape, y_test.shape)   # (10000, 28, 28)                     (10000,)  > 0 ~ 9 다중 분류
+
+# print(x_train[0])   
+# print("y_train[0] : " , y_train[0])   # 5
+# print(x_train[0].shape)               # (28, 28)
+
+# plt.imshow(x_train[0], 'gray')  # 0 : black, ~255 : white (가로 세로 색깔)
+# plt.imshow(x_train[0]) # 색깔 지정 안해도 나오긴 함
+# plt.show()  
+
+# preprocessing
+x_train = x_train.reshape(60000, 28, 28, 1).astype('float32')/255. 
+# 4차원 만들어준다. float타입으로 바꾸겠다. -> /255. -> 0 ~ 1 사이로 수렴됨
+x_test = x_test.reshape(10000, 28, 28, 1)/255. 
+# x_test.reshape(x_test.shape[0], x_test.shape[1],x_test.shape[2],1)
+
+# y >> OnHotEncoding
+
+from sklearn.preprocessing import OneHotEncoder
+y_train = y_train.reshape(-1,1)
+y_test = y_test.reshape(-1,1)
+# print(y_train[0])       # [5]
+# print(y_train.shape)    # (60000, 1)
+# print(y_test[0])        # [7]
+# print(y_test.shape)     # (10000, 1)
+
+encoder = OneHotEncoder()
+encoder.fit(y_train)
+encoder.fit(y_test)
+y_train = encoder.transform(y_train).toarray()  #toarray() : list 를 array로 바꿔준다.
+y_test = encoder.transform(y_test).toarray()    #toarray() : list 를 array로 바꿔준다.
+# print(y_train)
+# print(y_test)
+# print(y_train.shape)    # (60000, 10)
+# print(y_test.shape)     # (10000, 10)
+
+
+# Modeling
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+
+es = EarlyStopping(monitor='val_loss', patience=5, mode='max')
+
+# 분산처리 : 그래픽 카드 두 장 이상인 경우
+import tensorflow as tf
+strategy = tf.distribute.MirroredStrategy(cross_device_ops=\
+    tf.distribute.HierarchicalCopyAllReduce()
+)
+
+# with 문 : 객체의 라이프사이클(생성 >> 사용 >> 소멸)
+# model ~ compile 까지 scope에 넣어준다. >> 분산해서 처리하겠다.
+with strategy.scope() :
+    model = Sequential()
+    model.add(Conv2D(filters=16, kernel_size=(2,2), padding='same', strides=1, input_shape=(28,28,1)))
+    model.add(MaxPooling2D(pool_size=2))
+    model.add(Dropout(0.1))
+    model.add(Conv2D(filters=16, kernel_size=(4,4), padding='same', strides=1))
+    model.add(MaxPooling2D(pool_size=3))
+    model.add(Dropout(0.1))
+    model.add(Flatten())
+
+    model.add(Dense(8))
+    model.add(Dense(10, activation='softmax'))
+
+    # Compile, Train
+    model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
+
+hist = model.fit(x_train, y_train, epochs=100, batch_size=32, validation_split=0.2, callbacks=[es])
+
+# Evaluate, Predict
+result = model.evaluate(x_test, y_test, batch_size=32)
+print("loss : ", result[0])
+print("accuracy : ", result[1])
+
+# 응용
+# y_test 10개와 y_test 10개를 출력
+# print("y_test[:10] :\n", y_test[:10])
+print("y_test[:10] :")
+print(np.argmax(y_test[:10],axis=1))
+
+y_predict = model.predict(x_test[:10])
+print("y_pred[:10] :")  
+print(np.argmax(y_predict,axis=1))
+
+
+# loss :  0.05317089334130287
+# accuracy :  0.9817000031471252
+# y_test[:10] :
+# [7 2 1 0 4 1 4 9 5 9]
+# y_pred[:10] :
+# [7 2 1 0 4 1 4 9 5 9]
