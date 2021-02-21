70d6b0ca21ac8883d48af7906cc5831ceef2f795
leejisue <jk04059@naver.com>
2021-02-21 15:43:07 +0900

    02/21

---
+++ python_import/p11_car.py
@@ -0,0 +1,7 @@
+def drive() :
+    print("운전하다.")
+
+drive()
+
+print("P11_car.py의 module 이름은 : ", __name__)    # __name__ : 모듈 이름을 알려줌.
+# P11_car.py의 module 이름은 :  __main__ <<  실행의 기준이 됨. 이 파일이 main
