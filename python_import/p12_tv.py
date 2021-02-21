e47304d52d841480f8ca90906c6160a970c9990f
leejisue <jk04059@naver.com>
2021-02-21 15:43:45 +0900

    02/21

---
+++ python_import/p12_tv.py
@@ -0,0 +1,7 @@
+def watch() :
+    print("시청하다.")
+
+watch()
+
+print("P12_tv.py의 module 이름은 : ", __name__)    # __name__ : 모듈 이름을 알려줌.
+# P12_tv.py의 module 이름은 :  __main__  << 실행의 기준이 됨. 이 파일이 main
