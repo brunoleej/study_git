bc4392da920580777544de8f2d9305f10b0ee1ef
leejisue <jk04059@naver.com>
2021-02-24 02:43:24 +0900

    2/24

---
+++ python_import/p62_import.py
@@ -0,0 +1,11 @@
+# 실제 파일 위치 :  c :\\ users\\ai\\anaconda3\\test_import\\ p62_import.py
+
+def sum2() :
+    print("작업 그룹 임포트 썸탄다!!!")
+
+print("이 파일은 아나콘다 폴더에서 들어있을 것이다.")
+
+
+if __name__ == '__main__' :
+    print("이것도 출력 되나?")
+
