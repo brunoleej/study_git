b9a71103555a27403202dea3e8af1cb76892fc03
leejisue <jk04059@naver.com>
2021-02-24 02:42:43 +0900

    02/24

---
+++ python_import/p61_sys.py
@@ -0,0 +1,9 @@
+# 
+
+import sys
+print(sys.path)
+# ['c:\\Study\\python_import', 'C:\\Users\\ai\\Anaconda3\\python38.zip', 'C:\\Users\\ai\\Anaconda3\\DLLs', '
+# C:\\Users\\ai\\Anaconda3\\lib', 'C:\\Users\\ai\\Anaconda3', 'C:\\Users\\ai\\Anaconda3\\lib\\site-packages', 
+# 'C:\\Users\\ai\\Anaconda3\\lib\\site-packages\\win32', 'C:\\Users\\ai\\Anaconda3\\lib\\site-packages\\win32\\lib', 
+# 'C:\\Users\\ai\\Anaconda3\\lib\\site-packages\\Pythonwin']
+
