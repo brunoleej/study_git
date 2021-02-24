038140a835bb156473a313c7c72e03f2802e14f7
leejisue <jk04059@naver.com>
2021-02-25 01:09:16 +0900

    02/25

---
+++ python_import/p74_main.py
@@ -0,0 +1,14 @@
+import p73_class as p73
+
+# 클래스를 임포트 할 수 있다.
+# malddong : 인스턴스를 생성한다.
+# __init__에 필요한 변수들을 넣어준다.
+malddong = p73.Person('maldong', 20, 'Seoul jongro')
+
+# 말동 클래스 안에 있는 함수를 실행시킨다.
+malddong.greeting() 
+
+
+
+# 파이썬 모듈 배포 참고 페이지 
+# https://docs.python.org/ko/3/distributing/index.html
