c3978a16d9d92fdb0ae9b9fb32b25a360a9dfa45
leejisue <jk04059@naver.com>
2021-03-01 20:47:58 +0900

    03/01

---
+++ data science/mathmatics/03/PCA3.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.decomposition import PCA
+
+iris = load_iris()
+
+N = 10 # 앞의 10송이만 선택
+X = iris.data[:N, :2] # 꽃받침 길이와 꽃받침 폭만 선택
+
+pca1 = PCA(n_components=1)
+X_low = pca1.fit_transform(X)
+X2 = pca1.inverse_transform(X_low)
+
+plt.figure(figsize=(7, 7))
+
+ax = sns.scatterplot(0, 1, data=pd.DataFrame(X), s=100, color=".2", marker="s")
+
+for i in range(N):
+    d = 0.03 if X[i, 1] > X2[i, 1] else -0.04
+    ax.text(X[i, 0] - 0.065, X[i, 1] + d, "표본 {}".format(i + 1))
+    plt.plot([X[i, 0], X2[i, 0]], [X[i, 1], X2[i, 1]], "k--")
+
+plt.plot(X2[:, 0], X2[:, 1], "o-", markersize=10)
+plt.plot(X[:, 0].mean(), X[:, 1].mean(), markersize=10, marker="D")
+
+plt.axvline(X[:, 0].mean(), c='r')
+plt.axhline(X[:, 1].mean(), c='r')
+
+plt.grid(False)
+
+plt.xlabel("꽃받침 길이")
+plt.ylabel("꽃받침 폭")
+
+plt.title("Iris 데이터의 1차원 차원축소")
+
+plt.axis("equal")
+
+plt.show()
