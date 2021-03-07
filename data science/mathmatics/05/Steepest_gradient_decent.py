31a6ba1eaf6493bbf4daf5846a86a996a3a52d06
leejisue <jk04059@naver.com>
2021-03-07 15:51:50 +0900

    03/07

---
+++ data science/mathmatics/05/Steepest_gradient_decent.py
@@ -0,0 +1,47 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def f1d(x):
+    """f1(x)의 도함수"""
+    return 2 * (x - 2.0)
+
+def f1(x):
+    return (x - 2) ** 2 + 2
+
+def f2(x, y):
+    return (1 - x)**2 + 100.0 * (y - x**2)**2
+
+xx = np.linspace(-1, 4, 100)
+
+plt.plot(xx, f1(xx), 'k-')
+# step size
+mu = 0.4
+# k = 0
+
+x = 0
+
+plt.plot(x, f1(x), 'go', markersize=10)
+plt.text(x + 0.1, f1(x) + 0.1, "1차 시도")
+plt.plot(xx, f1d(x) * (xx - x) + f1(x), 'b--')
+print("1차 시도: x_1 = {:.2f}, g_1 = {:.2f}".format(x, f1d(x)))
+# k = 1
+
+x = x - mu * f1d(x)
+
+plt.plot(x, f1(x), 'go', markersize=10)
+plt.text(x - 0.2, f1(x) + 0.4, "2차 시도")
+plt.plot(xx, f1d(x) * (xx - x) + f1(x), 'b--')
+print("2차 시도: x_2 = {:.2f}, g_2 = {:.2f}".format(x, f1d(x)))
+# k = 2
+
+x = x - mu * f1d(x)
+
+plt.plot(x, f1(x), 'go', markersize=10)
+plt.text(x - 0.2, f1(x) - 0.7, "3차 시도")
+plt.plot(xx, f1d(x) * (xx - x) + f1(x), 'b--')
+print("3차 시도: x_3 = {:.2f}, g_3 = {:.2f}".format(x, f1d(x)))
+plt.xlabel("x")
+plt.ylabel("$f_1(x)$")
+plt.title("최대경사법을 사용한 1차함수의 최적화")
+plt.ylim(0, 10)
+plt.show()
