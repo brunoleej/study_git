3a0ea6bc490d7635175656011bfa83825befd2ad
leejisue <jk04059@naver.com>
2021-03-11 20:15:29 +0900

    03/11

---
+++ data science/mathmatics/06/Empty_set.py
@@ -0,0 +1,10 @@
+A1 = set([1, 2, 3, 4])
+A2 = set([2, 4, 6])
+A3 = set([1, 2, 3])
+A4 = set([2, 3, 4, 5, 6])
+
+empty_set = set([])
+print(empty_set)        # set()
+print(empty_set < A1)   # True
+print(empty_set < A1)   # True
+print(empty_set < A1)   # True
