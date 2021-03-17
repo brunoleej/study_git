ed3ba15034b5aed10c23d8fefc1a710b12ca8700
leejisue <jk04059@naver.com>
2021-03-17 21:09:41 +0900

    03/17

---
+++ data science/mathmatics/08/normal_distribution.py
@@ -0,0 +1,16 @@
+mu = 0
+std = 1
+rv = sp.stats.norm(mu, std)
+
+xx = np.linspace(-5, 5, 100)
+plt.plot(xx, rv.pdf(xx))
+plt.arrow(0, 0.05, 2, 0, lw=3, color='r', head_width=0.02, head_length=0.2, length_includes_head=True)
+plt.arrow(0, 0.05, -2, 0, lw=3, color='r', head_width=0.02, head_length=0.2, length_includes_head=True)
+plt.text(-0.95, 0.03, "표준편차의 약 4배")
+plt.ylabel("p(x)")
+plt.title("정규분포의 확률밀도함수")
+plt.show()
+
+np.random.seed(0)
+x = rv.rvs(20)
+print(x)
