93ab47e5384f101195f3507d702e4ace53c3e1f4
leejisue <jk04059@naver.com>
2021-03-21 15:11:30 +0900

    03/21

---
+++ data science/mathmatics/09/Moment_estimation.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def estimate_beta(x):
+    x_bar = x.mean()
+    s2 = x.var()
+    a = x_bar * (x_bar * (1 - x_bar) / s2 - 1)
+    b = (1 - x_bar) * (x_bar * (1 - x_bar) / s2 - 1)
+    return a, b
+params = estimate_beta(x)
+print(params)
+
+xx = np.linspace(0, 1, 1000)
+sns.distplot(x, kde=False, norm_hist=True)
+plt.plot(xx, sp.stats.beta(params[0], params[1]).pdf(xx))
+plt.xlim(0, 1)
+plt.title("베타 분포를 따르는 표본의 히스토그램과 추정된 확률밀도함수")
+plt.show()
