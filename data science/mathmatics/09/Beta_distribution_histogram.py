a17af90d6cfee959903261195ebbf08dff7c1ef1
leejisue <jk04059@naver.com>
2021-03-21 15:06:47 +0900

    03/21

---
+++ data science/mathmatics/09/Beta_distribution_histogram.py
@@ -0,0 +1,8 @@
+import numpy as np
+import scipy as sp
+
+np.random.seed(0)
+x = sp.stats.beta(15, 12).rvs(10000)
+sns.distplot(x, kde=False, norm_hist=True)
+plt.title("베타 분포를 따르는 표본의 히스토그램")
+plt.show()
