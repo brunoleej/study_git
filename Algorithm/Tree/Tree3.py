dbb9e876edee6970d5bd2574146b2aa8e7ed36d5
leejisue <jk04059@naver.com>
2021-04-26 23:06:18 +0900

    04/26

---
+++ Algorithm/Tree/Tree3.py
@@ -0,0 +1,49 @@
+# Binary Search Tree 탐색class NodeMgmt:
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left, self.right = None, None  # 이렇게도 한번에 여러 변수를 초기화할 수 있음
+
+class NodeMgmt:
+    def __init__(self, head):
+        self.head = head
+
+    def insert(self, value):
+        self.current_node = self.head
+        while True:
+            if value < self.current_node.value:
+                if self.current_node.left != None:
+                    self.current_node = self.current_node.left
+                else:
+                    self.current_node.left = Node(value)
+                    break
+            else:
+                if self.current_node.right != None:
+                    self.current_node = self.current_node.right
+                else:
+                    self.current_node.right = Node(value)
+                    break
+    
+    def search(self, value):
+        self.current_node = self.head
+        while self.current_node:
+            if self.current_node.value == value:
+                return True
+            elif value < self.current_node.value:
+                self.current_node = self.current_node.left
+            else:
+                self.current_node = self.current_node.right
+        return False
+
+head = Node(1)
+BST = NodeMgmt(head)
+BST.insert(2)
+BST.insert(3)
+BST.insert(0)
+BST.insert(4)
+BST.insert(8)
+
+# 탐색
+print(BST.search(8))    # True
+print(BST.search(2))    # True
+print(BST.search(-2))   # False
