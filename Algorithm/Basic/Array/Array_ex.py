aa0b0f108e8da7d348625c93ed57261d3aea6083
leejisue <jk04059@naver.com>
2021-04-27 20:47:41 +0900

    04/27

---
+++ Algorithm/Basic/Array/Array_ex.py
@@ -0,0 +1,49 @@
+# 연습 1
+# 위의 2차원 배열에서 9, 8, 7 순서로 출력해보기
+data = [[1,2,3],[4,5,6],[7,8,9]]
+
+print(data[-1][-1]) # 9
+print(data[-1][-2]) # 8
+print(data[-1][-3]) # 7
+
+# 연습 2
+# 아래의 dataset에서 전체 이름 안에 M이 몇번 나왔는지 빈도수 출력하기
+dataset = ['Braund, Mr. Owen Harris',
+'Cumings, Mrs. John Bradley (Florence Briggs Thayer)',
+'Heikkinen, Miss. Laina',
+'Futrelle, Mrs. Jacques Heath (Lily May Peel)',
+'Allen, Mr. William Henry',
+'Moran, Mr. James',
+'McCarthy, Mr. Timothy J',
+'Palsson, Master. Gosta Leonard',
+'Johnson, Mrs. Oscar W (Elisabeth Vilhelmina Berg)',
+'Nasser, Mrs. Nicholas (Adele Achem)',
+'Sandstrom, Miss. Marguerite Rut',
+'Bonnell, Miss. Elizabeth',
+'Saundercock, Mr. William Henry',
+'Andersson, Mr. Anders Johan',
+'Vestrom, Miss. Hulda Amanda Adolfina',
+'Hewlett, Mrs. (Mary D Kingcome) ',
+'Rice, Master. Eugene',
+'Williams, Mr. Charles Eugene',
+'Vander Planke, Mrs. Julius (Emelia Maria Vandemoortele)',
+'Masselmani, Mrs. Fatima',
+'Fynney, Mr. Joseph J',
+'Beesley, Mr. Lawrence',
+'McGowan, Miss. Anna "Annie"',
+'Sloper, Mr. William Thompson',
+'Palsson, Miss. Torborg Danira',
+'Asplund, Mrs. Carl Oscar (Selma Augusta Emilia Johansson)',
+'Emir, Mr. Farred Chehab',
+'Fortune, Mr. Charles Alexander',
+'Dwyer, Miss. Ellen "Nellie"',
+'Todoroff, Mr. Lalio']
+
+sum_char = 0
+
+for num in dataset:
+    for index in range(len(num)):
+        if num[index] == 'M':
+            sum_char += 1
+
+print(sum_char) # 38
