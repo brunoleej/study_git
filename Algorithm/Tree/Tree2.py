8f47d0af31e2b30d487fc13bfffb70d0ea4039a8
leejisue <jk04059@naver.com>
2021-04-29 22:57:26 +0900

    04/29

---
+++ Algorithm/Tree/Tree2.py
@@ -0,0 +1,32 @@
+# 이진 탐색 트리에 데이터 넣기
+    # 이진 탐색 트리 조건에 부합하게 데이터를 넣어야 함
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left, self.right = None, None
+
+class NodeMgmt:
+    def __init__(self, head):
+        self.head = head    # head는 root 노드
+
+    def insert(self, value):
+        self.current_node = self.head
+        while True:
+            if value < self.current_node.value:
+                if self.current_node.left != None:
+                    self.current_node = self.current_node.left
+                else:
+                    self.current_node.left = Node(value)
+                    break
+            else:
+                if self.current_node.right != None:
+                    self.current_node = self.current_node.right
+                else:
+                    self.current_node.right = Node(value)
+                    break
+                
+head = Node(1)
+BST = NodeMgmt(head)
+BST.insert(2)
+
