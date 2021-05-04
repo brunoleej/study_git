01804c7d68c3dce06f519b4936d7fbb51db6995f
leejisue <jk04059@naver.com>
2021-05-04 20:54:57 +0900

    05/04

--- Algorithm/Basic/First-class function/First_class_func2.py
+++ Algorithm/Basic/First-class function/First_class_func2.py
@@ -1,5 +1,4 @@
 # 2. func1 이라는 변수는 calc_square 함수를 가리키고, calc_square 와 마찬가지로 인자도 넣어서 결과도 얻을 수 있음 (완전 calc_square와 동일)
-
 print(func1)    # 4
 func1(2)    # <function calc_square at 0x000001C5A3627160>
 
