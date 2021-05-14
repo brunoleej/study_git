3f3339567cdc3323aaa6275ecb73bd3fbc04f55a
leejisue <jk04059@naver.com>
2021-05-14 21:50:39 +0900

    05/14

--- Reinforcement Learning/Project/MarioBros/Mario_test.py
+++ Reinforcement Learning/Project/MarioBros/Mario_test.py
@@ -5,7 +5,6 @@ from nes_py.app import JoypadSpace
 from gym_super_mario_bros.actions import RIGHT_ONLY
 from wrappers import wrapper
 
-
 env = gym_super_mario_bros.make('SuperMarioBros-1-1-v0')
 env = JoypadSpace(env.RIGHT_ONLY)
 env = wrapper(env)
