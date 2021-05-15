03146310fa56d211fba70d78bee2343f8ac703f3
leejisue <jk04059@naver.com>
2021-05-15 21:26:51 +0900

    05/15

--- Reinforcement Learning/Q_learning/01_CarRacing_v0.py
+++ Reinforcement Learning/Q_learning/01_CarRacing_v0.py
@@ -1,9 +1,12 @@
 # Library 추가
 import gym
+
 # make 함수로 simulation instance 생성
 env = gym.make('CarRacing-v0')
+
 # reset함수를 통해 환경을 초기화
 env.reset()
+
 # 반복문을 통해 매 time-step마다 환경을 rendering합니다.
 for _ in range(10000):
     env.render()
