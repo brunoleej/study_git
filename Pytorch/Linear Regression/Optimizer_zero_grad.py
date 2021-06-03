306f37ec14619a498ea2f45fd15d507c72e4e425
leejisue <jk04059@naver.com>
2021-06-03 14:01:50 +0900

    06/03

--- Pytorch/Linear Regression/Optimizer_zero_grad.py
+++ Pytorch/Linear Regression/Optimizer_zero_grad.py
@@ -9,5 +9,4 @@ for epochs in range(EPOCHS + 1):
     z = 2 * w
     z.backward()
     print('수식을 w로 미분한 값 : {}'.format(w.grad))
-
 # 계속해서 미분값인 2가 누적되는 것을 볼 수 있습니다. 그렇기 때문에 optimizer.zero_grad()를 통해 미분값을 계속 0으로 초기화시켜줘야 합니다.
