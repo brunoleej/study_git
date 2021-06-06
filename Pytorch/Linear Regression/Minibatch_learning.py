6246c84a5151af609f44d73b00b98b6d39b3ec69
leejisue <jk04059@naver.com>
2021-06-06 21:23:10 +0900

    06/06

--- Pytorch/Linear Regression/Minibatch_learning.py
+++ Pytorch/Linear Regression/Minibatch_learning.py
@@ -23,7 +23,6 @@ from torch.utils.data import TensorDataset, DataLoader, dataloader
 x_train  =  torch.FloatTensor([[73,  80,  75], [93,  88,  93], [89,  91,  90], [96,  98,  100], [73,  66,  70]])  
 y_train  =  torch.FloatTensor([[152],  [185],  [180],  [196],  [142]])
 
-
 # TensorDataset의 입력으로 사용하고 dataset으로 저장
 dataset = TensorDataset(x_train,y_train)
 
