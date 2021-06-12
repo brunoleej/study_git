7692a5de24947436df07939372973a556c37bc2f
leejisue <jk04059@naver.com>
2021-06-12 20:05:58 +0900

    06/12

--- Pytorch/ANN/Multilayer_perceptron_digits2.py
+++ Pytorch/ANN/Multilayer_perceptron_digits2.py
@@ -8,7 +8,6 @@ digits = load_digits()
 X = digits.data
 Y = digits.target
 
-
 print("X Type: {}, Y Type: {}".format(X.dtype,Y.dtype)) # X Type: float64, Y Type: int32
 
 X = torch.tensor(X, dtype=torch.float32)
