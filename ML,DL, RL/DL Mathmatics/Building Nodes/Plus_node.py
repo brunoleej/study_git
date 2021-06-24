95be9295444017161a41364f0567b19d0c8c6486
leejisue <jk04059@naver.com>
2021-06-24 13:37:24 +0900

    06/24

---
+++ ML,DL, RL/DL Mathmatics/Building Nodes/Plus_node.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+class plus_Node:
+    def __init__(self):
+        self._x, self._y = None, None
+        self._z = None
+
+    def forward(self, x, y):    # forward propagation
+        self._x, self._y = x, y
+        self._z = self._x + self._y
+        return self._z
+
+    def backward(self, dz):
+        return dz, dz
+
+test_x = np.random.randn(5)
+print(test_x)
