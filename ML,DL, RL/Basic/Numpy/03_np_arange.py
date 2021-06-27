bc469daa89e175268284343171b972b570ad2efe
leejisue <jk04059@naver.com>
2021-06-28 02:38:55 +0900

    06/28

---
+++ ML,DL, RL/Basic/Numpy/03_np_arange.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+x1 = np.arange(10)
+print(x1)    # [0 1 2 3 4 5 6 7 8 9]
+
+x2 = np.arange(1,10)
+print(x2)   # [1 2 3 4 5 6 7 8 9]
+
+x3 = np.arange(1,10,2)
+print(x3)   # [1 3 5 7 9]
+
+x4 = np.arange(5,101,5)
+print(x4)
+'''
+[  5  10  15  20  25  30  35  40  45  50  55  60  65  70  75  80  85  90
+  95 100]
+'''
