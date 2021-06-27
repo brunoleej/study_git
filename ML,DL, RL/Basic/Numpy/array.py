748383a864defca44d4bbbbad237e7fcb3c02fcb
leejisue <jk04059@naver.com>
2021-06-28 02:38:21 +0900

    06/28

--- ML,DL, RL/Basic/Numpy/array.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-arr = np.array([1,2,3,4,5,6])
-print(arr)  # [1 2 3 4 5 6]
-
-print(arr.dtype)    # int32
-
-arr = np.array([1,2,3],dtype = float)
-print(arr.dtype)    # float64
--- ML,DL, RL/Basic/Numpy/asfarray.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-# dtype을 따로 설정하지 않아도 자동으로 float 형태로 바꾸면서 배열을 만든다
-e = [1,2,3,4,5]
-print(np.asfarray(e))   # [1. 2. 3. 4. 5.]
-print(np.asfarray(e).dtype) # float64
