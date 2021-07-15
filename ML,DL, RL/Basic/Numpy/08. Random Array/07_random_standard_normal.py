39bfff80c4754d34166ba18d6443e527116924db
leejisue <jk04059@naver.com>
2021-07-15 14:00:21 +0900

    07/15

---
+++ ML,DL, RL/Basic/Numpy/08. Random Array/07_random_standard_normal.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+a = np.random.standard_normal(3)
+print(a)    # [ 1.18562239 -0.28938639 -1.42368894]
+
+b = np.random.standard_normal((2, 3))
+print(b)
+'''
+[[ 0.51017293 -1.26144206 -0.8561379 ]
+ [-3.53233946 -0.57759275  1.56705291]]
+'''
