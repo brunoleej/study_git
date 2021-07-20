069df6a0c2b58f9e327e0e896edacd5d35369e97
leejisue <jk04059@naver.com>
2021-07-21 00:16:51 +0900

    07/21

---
+++ Basic/Matplotlib/04. Matplotlib Legend/06_legend_frames.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+plt.plot([1, 2, 3, 4], [2, 3, 5, 10], label='Price ($)')
+plt.plot([1, 2, 3, 4], [3, 5, 9, 7], label='Demand (#)')
+plt.xlabel('X-Axis')
+plt.ylabel('Y-Axis')
+# plt.legend(loc='best')
+plt.legend(loc='best', ncol=2, fontsize=14, frameon=True, shadow=True)
+
+plt.show()
