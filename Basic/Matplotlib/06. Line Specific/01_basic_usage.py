8ed4de65d1ae27f34fdff1b7cf05224ea0abb771
leejisue <jk04059@naver.com>
2021-07-22 01:48:40 +0900

    07/22

---
+++ Basic/Matplotlib/06. Line Specific/01_basic_usage.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+plt.plot([1, 2, 3], [4, 4, 4], '-', color='C0', label='Solid')
+plt.plot([1, 2, 3], [3, 3, 3], '--', color='C0', label='Dashed')
+plt.plot([1, 2, 3], [2, 2, 2], ':', color='C0', label='Dotted')
+plt.plot([1, 2, 3], [1, 1, 1], '-.', color='C0', label='Dash-dot')
+plt.xlabel('X-Axis')
+plt.ylabel('Y-Axis')
+plt.axis([0.8, 3.2, 0.5, 5.0])
+plt.legend(loc='upper right', ncol=4)
+plt.show()
