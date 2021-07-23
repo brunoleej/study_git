247a58ecf2a4a4dd5aee0d11c3e03535580b8376
leejisue <jk04059@naver.com>
2021-07-23 23:34:24 +0900

    07/23

---
+++ Basic/Matplotlib/07. Marker/03_marker_parameter.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+
+plt.plot([4, 5, 6], marker="H")
+plt.plot([3, 4, 5], marker="d")
+plt.plot([2, 3, 4], marker="x")
+plt.plot([1, 2, 3], marker=11)
+plt.plot([0, 1, 2], marker='$Z$')
+plt.show()
