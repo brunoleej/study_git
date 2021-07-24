515f84697160eff7ae1cd6918833454358137252
leejisue <jk04059@naver.com>
2021-07-24 16:35:15 +0900

    07/24

--- Basic/Matplotlib/07. Marker/02_line_marker.py
+++ Basic/Matplotlib/07. Marker/02_line_marker.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 
 # plt.plot([1, 2, 3, 4], [2, 3, 5, 10], 'bo-')    # blue + marker + string line
 plt.plot([1, 2, 3, 4], [2, 3, 5, 10], 'bo--')     # blue + marker + point line
+
 plt.xlabel('X-Axis')
 plt.ylabel('Y-Axis')
+
 plt.show()
