238d72aace5ecb9e0b8504d0a44ffb87b061eea1
leejisue <jk04059@naver.com>
2021-07-24 16:34:49 +0900

    07/24

--- Basic/Matplotlib/07. Marker/02_ex2.py
+++ Basic/Matplotlib/07. Marker/02_ex2.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 plt.plot([1, 2, 3, 4], [2.0, 3.0, 5.0, 10.0], color='limegreen')
 plt.plot([1, 2, 3, 4], [2.0, 2.8, 4.3, 6.5], color='violet')
 plt.plot([1, 2, 3, 4], [2.0, 2.5, 3.3, 4.5], color='dodgerblue')
+
 plt.xlabel('X-Axis')
 plt.ylabel('Y-Axis')
 
