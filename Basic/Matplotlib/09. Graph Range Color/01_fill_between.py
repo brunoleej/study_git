843d81fdad23d48f6c5f1bcedd50e0c2e6b803bb
leejisue <jk04059@naver.com>
2021-07-25 21:50:59 +0900

    07/25

---
+++ Basic/Matplotlib/09. Graph Range Color/01_fill_between.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4]
+y = [2, 3, 5, 10]
+
+plt.plot(x, y)
+plt.xlabel('X-Axis')
+plt.ylabel('Y-Axis')
+plt.fill_between(x[1:3], y[1:3], alpha=0.5)
+
+plt.show()
