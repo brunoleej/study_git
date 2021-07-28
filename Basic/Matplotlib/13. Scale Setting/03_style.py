12c7cf8f05462851163e469bc0f13a5af6573cfb
leejisue <jk04059@naver.com>
2021-07-29 00:12:24 +0900

    07/29

---
+++ Basic/Matplotlib/13. Scale Setting/03_style.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.arange(0, 2, 0.2)
+
+plt.plot(x, x, 'bo')
+plt.plot(x, x**2, color='#e35f62', marker='*', linewidth=2)
+plt.plot(x, x**3, color='springgreen', marker='^', markersize=9)
+plt.xticks(np.arange(0, 2, 0.2), labels=['Jan', '', 'Feb', '', 'Mar', '', 'May', '', 'June', '', 'July'])
+plt.yticks(np.arange(0, 7), ('0', '1GB', '2GB', '3GB', '4GB', '5GB', '6GB'))
+
+plt.tick_params(axis='x', direction='in', length=3, pad=6, labelsize=14, labelcolor='green', top=True)
+plt.tick_params(axis='y', direction='inout', length=10, pad=15, labelsize=12, width=2, color='r')
+
+plt.show()
