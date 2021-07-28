9e8667d0144209890190064b5c13005fb7a6e250
leejisue <jk04059@naver.com>
2021-07-29 00:11:39 +0900

    07/29

---
+++ Basic/Matplotlib/13. Scale Setting/01_basic_usage.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.arange(0, 2, 0.2)
+
+plt.plot(x, x, 'bo')
+plt.plot(x, x**2, color='#e35f62', marker='*', linewidth=2)
+plt.plot(x, x**3, color='forestgreen', marker='^', markersize=9)
+plt.xticks([0, 1, 2])
+plt.yticks(np.arange(1, 6))
+
+plt.show()
