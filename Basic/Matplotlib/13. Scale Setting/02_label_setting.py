f5f417255586966898be586b3b5ea731ae194b99
leejisue <jk04059@naver.com>
2021-07-29 00:11:54 +0900

    07/29

---
+++ Basic/Matplotlib/13. Scale Setting/02_label_setting.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.arange(0, 2, 0.2)
+
+plt.plot(x, x, 'bo')
+plt.plot(x, x**2, color='#e35f62', marker='*', linewidth=2)
+plt.plot(x, x**3, color='springgreen', marker='^', markersize=9)
+plt.xticks(np.arange(0, 2, 0.2), labels=['Jan', '', 'Feb', '', 'Mar', '', 'May', '', 'June', '', 'July'])
+plt.yticks(np.arange(0, 7), ('0', '1GB', '2GB', '3GB', '4GB', '5GB', '6GB'))
+
+plt.show()
