d5f2efd769574666a3369552ebed5b1a8ee40432
leejisue <jk04059@naver.com>
2021-07-30 11:48:01 +0900

    07/30

---
+++ Basic/Matplotlib/16. Bar Graph/01_basic_usage_1.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.arange(3)
+years = ['2018', '2019', '2020']
+values = [100, 400, 900]
+
+plt.bar(x, values)
+plt.xticks(x, years)
+
+plt.show()
