56798b139bba7a6c09119107ba810a0ea7235330
leejisue <jk04059@naver.com>
2021-08-03 22:29:53 +0900

    08/03

---
+++ Basic/Matplotlib/20. Histogram/03_cumulative_hist.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+weight = [68, 81, 64, 56, 78, 74, 61, 77, 66, 68, 59, 71,
+          80, 59, 67, 81, 69, 73, 69, 74, 70, 65]
+
+plt.hist(weight, cumulative=True, label='cumulative=True')
+plt.hist(weight, cumulative=False, label='cumulative=False')
+plt.legend(loc='upper left')
+plt.show()
