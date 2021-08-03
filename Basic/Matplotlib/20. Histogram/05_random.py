e6dee8398a86e278738e8684ed25f3e679e61641
leejisue <jk04059@naver.com>
2021-08-03 22:30:27 +0900

    08/03

---
+++ Basic/Matplotlib/20. Histogram/05_random.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+a = 2.0 * np.random.randn(10000) + 1.0
+b = np.random.standard_normal(10000)
+c = 20.0 * np.random.rand(5000) - 10.0
+
+plt.hist(a, bins=100, density=True, alpha=0.7, histtype='step')
+plt.hist(b, bins=50, density=True, alpha=0.5, histtype='stepfilled')
+plt.hist(c, bins=100, density=True, alpha=0.9, histtype='step')
+
+plt.show()
