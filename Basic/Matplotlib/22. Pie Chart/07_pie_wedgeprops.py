ea816f7d94f7c9654e7c5590902913b7d3dd50b8
leejisue <jk04059@naver.com>
2021-08-05 00:01:01 +0900

    08/05

---
+++ Basic/Matplotlib/22. Pie Chart/07_pie_wedgeprops.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+ratio = [34, 32, 16, 18]
+labels = ['Apple', 'Banana', 'Melon', 'Grapes']
+colors = ['#ff9999', '#ffc000', '#8fd9b6', '#d395d0']
+wedgeprops={'width': 0.7, 'edgecolor': 'w', 'linewidth': 5}
+
+plt.pie(ratio, labels=labels, autopct='%.1f%%', startangle=260, counterclock=False, colors=colors, wedgeprops=wedgeprops)
+plt.show()
