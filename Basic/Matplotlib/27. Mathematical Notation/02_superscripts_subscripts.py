71bec9f34668b73ec6929017aed44926c8861db6
leejisue <jk04059@naver.com>
2021-08-09 21:59:03 +0900

    08/09

---
+++ Basic/Matplotlib/27. Mathematical Notation/02_superscripts_subscripts.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+
+plt.plot([1, 2, 3, 4], [1, 4, 9, 16])
+plt.xlabel('X-Label')
+plt.ylabel('Y-Label')
+plt.text(1, 15, r'$\alpha^2 > \beta_5$', fontdict={'size': 16})
+
+plt.show()
