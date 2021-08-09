a561d45c3e96d60920f2ca27b7b3cca928e44329
leejisue <jk04059@naver.com>
2021-08-10 00:38:54 +0900

    08/10

---
+++ Basic/Matplotlib/28. Graph Style/03_graph_style_rcparams.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+plt.style.use('default')
+plt.rcParams['figure.figsize'] = (6, 3)
+plt.rcParams['font.size'] = 12
+# plt.rcParams['figure.figsize'] = (4, 3)
+# plt.rcParams['font.size'] = 14
+
+plt.plot([1, 2, 3, 4], [4, 6, 2, 7])
+plt.show()
