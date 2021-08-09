1a174e2056b3cf9c292b4eecd21797bed51120d5
leejisue <jk04059@naver.com>
2021-08-10 00:38:27 +0900

    08/10

---
+++ Basic/Matplotlib/28. Graph Style/01_graph_style_basic_usuage.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+plt.style.use('bmh')
+# plt.style.use('ggplot')
+# plt.style.use('classic')
+# plt.style.use('Solarize_Light2')
+# plt.style.use('default')
+
+plt.plot([1, 2, 3, 4], [4, 6, 2, 7])
+plt.show()
