c570d7f23016c8ff3329dfd071c7e112c39dea52
leejisue <jk04059@naver.com>
2021-08-11 00:06:18 +0900

    08/11

---
+++ Basic/Matplotlib/29. Image/01_image_basic_usuage.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x1 = np.linspace(0.0, 5.0)
+x2 = np.linspace(0.0, 2.0)
+
+y1 = np.cos(2 * np.pi * x1) * np.exp(-x1)
+y2 = np.cos(2 * np.pi * x2)
+
+plt.subplot(2, 1, 1)                # nrows=2, ncols=1, index=1
+plt.plot(x1, y1, 'o-')
+plt.title('1st Graph')
+plt.ylabel('Damped oscillation')
+
+plt.subplot(2, 1, 2)                # nrows=2, ncols=1, index=2
+plt.plot(x2, y2, '.-')
+plt.title('2nd Graph')
+plt.xlabel('time (s)')
+plt.ylabel('Undamped')
+
+plt.tight_layout()
+# plt.show()
+plt.savefig('savefig_default.png')
