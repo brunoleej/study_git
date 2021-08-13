9f32b193af80c743bb899610c338e44cb1b14073
leejisue <jk04059@naver.com>
2021-08-14 00:44:09 +0900

    08/14

---
+++ Optimization/01. Gradient Methods/Vanilla_gradient_descent1.py
@@ -0,0 +1,66 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+# Hyperparameter
+threshold = 0.0003   # convergence threshold
+step_size = 0.01     # step-size factor
+point_storage = []   # point history
+
+# initial point
+point = np.array([-2.0, 2.0])
+x1, x2 = point  # decision variable
+point_storage.append(point)
+
+def obj_func(x1, x2):
+    f_x = 1 + 2*x1 + 3*(x1**2 + x2**2) + 3*x1*x2
+    return f_x
+
+def g(x):
+    x1, x2 = x
+    f_x1 = 6*x1 + 4*x2 + 2
+    f_x2 = 4*x1 + 6*x2
+    return np.array([f_x1, f_x2])
+
+# Gradient Descent
+i = 0
+while True:
+    i += 1
+    point = point - step_size * g(point)
+    point_storage.append(point)
+
+    print("Point History : {}".format(point_storage))
+    print("Next Point : {}\n".format(point))
+    
+    compare_value = np.linalg.norm(point_storage[i] - point_storage[i-1])
+    
+    if compare_value < threshold:
+        print('First two Norm : {}'.format(np.linalg.norm(point_storage[1] - point_storage[0])))
+        print("Convergence Norm : {}".format(compare_value))
+        print("Convergence at {} iterations".format(i))
+        break     
+        
+# plotting 3d graph
+fig = plt.figure(figsize=(10,10))
+ax = fig.add_subplot(111, projection='3d')
+title = ax.set_title('objective_function')
+
+x1 = np.arange(-2.5, 2.5, 0.1)
+x2 = np.arange(-2.5, 2.5, 0.1)
+
+X1, X2 = np.meshgrid(x1, x2)
+graph = ax.plot_surface(X1, X2, obj_func(X1, X2),cmap=cm.coolwarm, alpha=.5)
+
+point_storage_np = np.array(point_storage)
+computed_x1 = point_storage_np[:, 0]
+computed_x2 = point_storage_np[:, 1]
+computed_z = obj_func(computed_x1, computed_x2)
+graph2 = ax.scatter(computed_x1, computed_x2, computed_z, color='black', linewidth = 0.01)
+plt.show()
+    
+'''
+Prespecified threshold : 0.0003
+First two Norm : 0.044721359549995836
+Convergence Norm : 0.0002946122988598515
+Convergence at 247 iterations
+'''
