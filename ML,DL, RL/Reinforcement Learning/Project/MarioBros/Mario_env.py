07c5b96992294b9b1d9bc3f0b1450d470d029a95
leejisue <jk04059@naver.com>
2021-08-26 00:28:13 +0900

    0268

--- ML,DL, RL/Reinforcement Learning/Project/MarioBros/Mario_env.py
+++ ML,DL, RL/Reinforcement Learning/Project/MarioBros/Mario_env.py
@@ -6,7 +6,6 @@ env.reset()
 print(env.action_space.n)   # 256
 print(env.observation_space.shape)  # (240, 256, 3)
 
-
 for _ in range(10):
     observation, reward, done, info = env.step(env.action_space.sample())
     env.render()
